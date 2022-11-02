76482fb55310b977bdb83d454457e0d0678653e9
sanghunDev <bsh20057@gmail.com>
2022-11-02 12:40:42 +0900

    perf(냄새2): 메서드 올리기(Pull Up Method) - 메서드를 하위 클래스에서 상위 클래스로 올리는 리팩토링 (상속 구조에서 하위 클래스의 중복 코드를 찾아서 상위로 올릴 수 있다) - 중복 코드는 버그 발생 여지가 많다 (A 코드는 바꾸고 B는 바꾸지 않은 경우)

--- src/main/java/me/whiteship/refactoring/_02_duplicated_code/_06_pull_up_method/Dashboard.java
+++ src/main/java/me/whiteship/refactoring/_02_duplicated_code/_06_pull_up_method/Dashboard.java
@@ -1,6 +1,12 @@
 package me.whiteship.refactoring._02_duplicated_code._06_pull_up_method;
 
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Dashboard {
 
@@ -9,6 +15,20 @@ public class Dashboard {
         reviewerDashboard.printReviewers();
 
         ParticipantDashboard participantDashboard = new ParticipantDashboard();
-        participantDashboard.printParticipants(15);
+        participantDashboard.printUserNames(15);
+    }
+
+    public void printUserNames(int eventId) throws IOException {
+        // Get github issue to check homework
+        GitHub gitHub = GitHub.connect();
+        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+        GHIssue issue = repository.getIssue(eventId);
+
+        // Get userNames
+        Set<String> userNames = new HashSet<>();
+        issue.getComments().forEach(c -> userNames.add(c.getUserName()));
+
+        // Print userNames
+        userNames.forEach(System.out::println);
     }
 }
