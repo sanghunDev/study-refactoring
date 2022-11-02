d137665b75aca46d05330349baa1bc3903b5b036
sanghunDev <bsh20057@gmail.com>
2022-11-02 12:19:27 +0900

    perf(냄새2): 함수 추출하기 - 주석이 달려있는 코드를 함수 추출하기 사용  (주석 제거 및 재사용 가능하게 처리) - 함수 이름만 보고도 함수의 역할을 알 수 있기 때문에 코드의 가독성이 좋아졌다

--- src/main/java/me/whiteship/refactoring/_02_duplicated_code/_04_extract_function/StudyDashboard.java
+++ src/main/java/me/whiteship/refactoring/_02_duplicated_code/_04_extract_function/StudyDashboard.java
@@ -9,33 +9,32 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class StudyDashboard {
-
     private void printParticipants(int eventId) throws IOException {
-        // Get github issue to check homework
-        GitHub gitHub = GitHub.connect();
-        GHRepository repository = gitHub.getRepository("whiteship/live-study");
-        GHIssue issue = repository.getIssue(eventId);
-
-        // Get participants
-        Set<String> participants = new HashSet<>();
-        issue.getComments().forEach(c -> participants.add(c.getUserName()));
-
-        // Print participants
-        participants.forEach(System.out::println);
+        GHIssue issue = getGhIssue(eventId);
+        Set<String> participants = getUserNames(issue);
+        print(participants);
     }
 
     private void printReviewers() throws IOException {
-        // Get github issue to check reviews
+        GHIssue issue = getGhIssue(30);
+        Set<String> reviewers = getUserNames(issue);
+        print(reviewers);
+    }
+
+    private void print(Set<String> participants) {
+        participants.forEach(System.out::println);
+    }
+
+    private Set<String> getUserNames(GHIssue issue) throws IOException {
+        Set<String> userNames = new HashSet<>();
+        issue.getComments().forEach(c -> userNames.add(c.getUserName()));
+        return userNames;
+    }
+
+    private GHIssue getGhIssue(int eventId) throws IOException {
         GitHub gitHub = GitHub.connect();
         GHRepository repository = gitHub.getRepository("whiteship/live-study");
-        GHIssue issue = repository.getIssue(30);
-
-        // Get reviewers
-        Set<String> reviewers = new HashSet<>();
-        issue.getComments().forEach(c -> reviewers.add(c.getUserName()));
-
-        // Print reviewers
-        reviewers.forEach(System.out::println);
+        return repository.getIssue(eventId);
     }
 
     public static void main(String[] args) throws IOException {
