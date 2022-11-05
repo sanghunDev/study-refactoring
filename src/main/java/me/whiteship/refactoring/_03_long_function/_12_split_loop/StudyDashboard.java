a3a7baaa32517977a231d6db1069fe7466f81891
sanghun <bsh20057@gmail.com>
2022-11-05 23:55:19 +0900

    perf(냄새3):반복문 쪼개기 (Split Loop) - 하나의 반복문에서 여러 다른 작업을 하는 코드를 쉽게 찾아볼 수 있다 - 해당 반복문을 수정할 때 여러 작업을 모두 고려하며 코딩을 해야한다 - 반복문을 여러개로 쪼개면 보다 쉽게 이해하고 수정할 수 있다 - 성능 문제를 야기할 수 있지만 리팩토링은 성능과 별개다, 리팩토링 후에도 성능 최적화 시도가 가능하다

--- src/main/java/me/whiteship/refactoring/_03_long_function/_12_split_loop/StudyDashboard.java
+++ src/main/java/me/whiteship/refactoring/_03_long_function/_12_split_loop/StudyDashboard.java
@@ -45,20 +45,8 @@ public class StudyDashboard {
                     try {
                         GHIssue issue = ghRepository.getIssue(eventId);
                         List<GHIssueComment> comments = issue.getComments();
-                        Date firstCreatedAt = null;
-                        Participant first = null;
-
-                        for (GHIssueComment comment : comments) {
-                            Participant participant = findParticipant(comment.getUserName(), participants);
-                            participant.setHomeworkDone(eventId);
-
-                            if (firstCreatedAt == null || comment.getCreatedAt().before(firstCreatedAt)) {
-                                firstCreatedAt = comment.getCreatedAt();
-                                first = participant;
-                            }
-                        }
-
-                        firstParticipantsForEachEvent[eventId - 1] = first;
+                        checkHomework(comments, participants, eventId);
+                        firstParticipantsForEachEvent[eventId - 1] = findFirst(comments);
                         latch.countDown();
                     } catch (IOException e) {
                         throw new IllegalArgumentException(e);
@@ -72,6 +60,27 @@ public class StudyDashboard {
 
         new StudyPrinter(this.totalNumberOfEvents, this.participants).execute();
         printFirstParticipants();
+    }
+
+    private Participant findFirst(List<GHIssueComment> comments) throws IOException {
+        Date firstCreatedAt = null;
+        Participant first = null;
+        for (GHIssueComment comment : comments) {
+            Participant participant = findParticipant(comment.getUserName(), participants);
+
+            if (firstCreatedAt == null || comment.getCreatedAt().before(firstCreatedAt)) {
+                firstCreatedAt = comment.getCreatedAt();
+                first = participant;
+            }
+        }
+        return first;
+    }
+
+    private void checkHomework(List<GHIssueComment> comments, List<Participant> participants, int eventId) {
+        for (GHIssueComment comment : comments) {
+            Participant participant = findParticipant(comment.getUserName(), participants);
+            participant.setHomeworkDone(eventId);
+        }
     }
 
     private void printFirstParticipants() {
