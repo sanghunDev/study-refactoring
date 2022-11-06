dab8510869cc1b10d6c02cd222dba9a668d8cefb
sanghun <bsh20057@gmail.com>
2022-11-06 23:56:01 +0900

    perf(냄새5 전역 데이터): 변수 캡슐화 하기 (Encapsulate Variable) - 전역 데이터(java public static 변수)는 아무곳에서나 변경될 수 있다는 문제가 있으며 어떤 코드로 인해 값이 바뀐 것인지 파악하기 어렵다 - 클래스 변수 (필드)도 비슷한 문제를 겪을 수 있다 - 변수 캡슐화하기를 적용해서 접근을 제어하거나 어디서 사용하는지 파악하기 쉽게 만들 수 있다 ( - 메소드는 점진적으로 새로운 메소드로 변경 가능, 데이터는 한번에 변경해야 함 - 데이터 구조를 변경하는 작업을 그보다는 조금 더 수월한 메소드 구조 변경 작업으로 대체할 수 있다 - 데이터가 사용되는 범위가 클수록 캡슐화를 하는 것이 더 중요해진다 - 함수를 사용해서 값을 변경하면 보다 쉽게 검증 로직을 추가하거나 변경에 따르는 후속 작업을 추가하는 것이 편리하다 - 불편 데이터의 경우 이런 리팩토링을 적용할 필요가 없다 )

--- src/main/java/me/whiteship/refactoring/_05_global_data/_17_encapsulate_variable/Thermostats.java
+++ src/main/java/me/whiteship/refactoring/_05_global_data/_17_encapsulate_variable/Thermostats.java
@@ -2,12 +2,47 @@ package me.whiteship.refactoring._05_global_data._17_encapsulate_variable;
 
 public class Thermostats {
 
-    public static Integer targetTemperature = 70;
+    /**
+     * static 키워드를 사용할때는 final 을 사용해 읽기 전용으로 사용하자
+     * 아니면 데이터에 직접 접근을 하는것이 아닌 메서드를 생성해서 참조하게 만들자
+     */
+    private static Integer targetTemperature = 70;
 
-    public static Boolean heating = true;
+    private static Boolean heating = true;
 
-    public static Boolean cooling = false;
+    private static Boolean cooling = false;
 
-    public static Boolean readInFahrenheit = true;
+    private static Boolean readInFahrenheit = true;
 
+    public static Integer getTargetTemperature() {
+        return targetTemperature;
+    }
+
+    public static void setTargetTemperature(Integer targetTemperature) {
+        Thermostats.targetTemperature = targetTemperature;
+    }
+
+    public static Boolean getHeating() {
+        return heating;
+    }
+
+    public static void setHeating(Boolean heating) {
+        Thermostats.heating = heating;
+    }
+
+    public static Boolean getCooling() {
+        return cooling;
+    }
+
+    public static void setCooling(Boolean cooling) {
+        Thermostats.cooling = cooling;
+    }
+
+    public static Boolean getReadInFahrenheit() {
+        return readInFahrenheit;
+    }
+
+    public static void setReadInFahrenheit(Boolean readInFahrenheit) {
+        Thermostats.readInFahrenheit = readInFahrenheit;
+    }
 }
