3ef8074afa3ad9bd25bc80bd069f0d8b946ab17b
sanghunDev <bsh20057@gmail.com>
2022-11-11 22:48:38 +0900

    perf(냄새8 산탄총 수술): 클래스 인라인(Inline Class) - 클래스 추출하기 (Extract Class)의 반대에 해당하는 리팩토링 - 리팩토링을 하는 중에 클래스의 책임을 옮기다보면 클래스의 존재 이유가 빈약해지는 경우가 발생할 수 있다 - 두개의 클래스를 여러 클래스로 나누는 리팩토링을 하는 경우에, 우선 클래스 인라인을 적용해서 두 클래스의 코드를 한 곳으로 모으고 그런 다음에 클래스 추출하기를 적용해서 새롭게 분리하는 리팩토링을 적용할 수 있다

--- src/main/java/me/whiteship/refactoring/_08_shotgun_surgery/_29_inline_class/Shipment.java
+++ src/main/java/me/whiteship/refactoring/_08_shotgun_surgery/_29_inline_class/Shipment.java
@@ -2,21 +2,39 @@ package me.whiteship.refactoring._08_shotgun_surgery._29_inline_class;
 
 public class Shipment {
 
-    private TrackingInformation trackingInformation;
+    /**
+     * 인라인 클래스 리팩토링 사용
+     * TrackingInformation 클래스에서 사용하던 부분을 제거, TrackingInformation 클래스는 불필요하게 됨
+     */
 
-    public Shipment(TrackingInformation trackingInformation) {
-        this.trackingInformation = trackingInformation;
-    }
 
-    public TrackingInformation getTrackingInformation() {
-        return trackingInformation;
-    }
+    private String shippingCompany;
 
-    public void setTrackingInformation(TrackingInformation trackingInformation) {
-        this.trackingInformation = trackingInformation;
+    private String trackingNumber;
+
+    public Shipment(String shippingCompany, String trackingNumber) {
+        this.shippingCompany = shippingCompany;
+        this.trackingNumber = trackingNumber;
     }
 
     public String getTrackingInfo() {
-        return this.trackingInformation.display();
+        return this.shippingCompany + ": " + this.trackingNumber;
     }
+
+    public String getShippingCompany() {
+        return shippingCompany;
+    }
+
+    public void setShippingCompany(String shippingCompany) {
+        this.shippingCompany = shippingCompany;
+    }
+
+    public String getTrackingNumber() {
+        return trackingNumber;
+    }
+
+    public void setTrackingNumber(String trackingNumber) {
+        this.trackingNumber = trackingNumber;
+    }
+
 }
