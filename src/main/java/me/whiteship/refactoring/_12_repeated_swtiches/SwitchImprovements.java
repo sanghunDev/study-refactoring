07c76b00f4910fc3d122e8c76ba33b15049cc789
sanghun <bsh20057@gmail.com>
2022-11-13 03:08:39 +0900

    perf(냄새12): 반복되는 switch (Repeated Switches) - 예전에는 switch 문이 한번만 등장해도 코드 냄새로 생각하고 다형성 적용을 권장 - 하지만 최근에는 다형성이 꽤 널리 사용되고 있으며, 여러 프로그래밍 언어에서 보다 세련된 형태의 switch 문을 지원하고 있다 - 요즘은 반복해서 등장하는 "동일한 switch 문"을 냄새로 여기고 있다 - 반복해서 동일한 switch 문이 존재할 경우, 새로운 조건을 추가하거나 기존의 조건을 변경할때 모든 switch 문을 찾아서 코드를 고쳐야 할지도 모른다
    - switch 문의 제거는 앞에서 알아봤기 때문에 여기선 switch 표현식에 관해서 살펴본다 java 14부터 가능(LTS 17)

--- src/main/java/me/whiteship/refactoring/_12_repeated_swtiches/SwitchImprovements.java
+++ src/main/java/me/whiteship/refactoring/_12_repeated_swtiches/SwitchImprovements.java
@@ -2,14 +2,27 @@ package me.whiteship.refactoring._12_repeated_swtiches;
 
 public class SwitchImprovements {
 
+    //기존의 switch 문법
     public int vacationHours(String type) {
         int result;
         switch (type) {
-            case "full-time": result = 120;
-            case "part-time": result = 80;
-            case "temporal": result = 32;
+            case "full-time": result = 120; break;
+            case "part-time": result = 80; break;
+            case "temporal": result = 32; break;
             default: result = 0;
         }
+        return result;
+    }
+
+    //변경된 switch 표현식
+    public int switchExpression(String type) {
+        // switch 문과는 다르며 결과를 담을 수 있다 (기존 코드의 break 부분도 제거 가능)
+        int result = switch (type) {
+            case "full-time" -> 120;
+            case "part-time" -> 80;
+            case "temporal" -> 32;
+            default -> 0;
+        };
         return result;
     }
 }
