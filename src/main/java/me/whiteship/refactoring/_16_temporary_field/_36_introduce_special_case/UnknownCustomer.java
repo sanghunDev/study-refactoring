13d82916f29a026cdb4f715deb83b07d51049401
sanghun <bsh20057@gmail.com>
2022-11-13 03:59:19 +0900

    perf(냄새16 임시 필드): 특이 케이스 추가하기(Introduce Special Case) - UnknownCustomer 클래스에 메소드를 추가해 해당 역할에 맞는 로직을 실행하도록 처리CustomerService 클래스의 조건문을 제거했다

--- src/main/java/me/whiteship/refactoring/_16_temporary_field/_36_introduce_special_case/UnknownCustomer.java
+++ src/main/java/me/whiteship/refactoring/_16_temporary_field/_36_introduce_special_case/UnknownCustomer.java
@@ -2,11 +2,16 @@ package me.whiteship.refactoring._16_temporary_field._36_introduce_special_case;
 
 public class UnknownCustomer extends Customer{
     public UnknownCustomer() {
-        super("unknown", null, null);
+        super("unknown", new BasicBillingPlan(), null);
     }
 
     @Override
     public boolean isUnknown() {
         return true;
+    }
+
+    @Override
+    public String getName() {
+        return "occupant";
     }
 }
