6007bef2a86d84519f30937a47738f98f456ecb0
sanghun <bsh20057@gmail.com>
2022-11-13 03:44:26 +0900

    perf(냄새15 추측성 일반화): 죽은 코드 제거하기(Remove Dead Code) - 추측성 일반화 : 나중에 어떠한 기능이 생길 것으로 예상해 여러 경우에 필요한 기능을 만들어놨지만 결국 쓰이지 않는 코드가 발생한 경우 - XP의 YAGNI (You aren't gonna need it) 원칙을 따르자 (지금 당장 필요하지 않으면 말들지 말자)
    - 죽은 코드 제거하기는 실제로 사용되지 않는 코드를 제거하는것
    (불필요한 코드를 남겨놓지 말자)

--- src/main/java/me/whiteship/refactoring/_15_speculative_generality/_35_remove_dead_code/Reservation.java
+++ src/main/java/me/whiteship/refactoring/_15_speculative_generality/_35_remove_dead_code/Reservation.java
@@ -2,6 +2,12 @@ package me.whiteship.refactoring._15_speculative_generality._35_remove_dead_code
 
 import java.time.LocalDateTime;
 
+/**
+ * 사용하지 않는 코드가 애플리케이션 성능이나 기능에 영향을 끼치지는 않는다
+ * 하지만 해당 소프트웨어가 어떻게 동작하는지 이해하려는 사람들에게는 꽤 고통을 줄 수 있다
+ * 실제로 나중에 필요해질 코드라 하더라도 지금 쓰이지 않는 코드라면 주석으로 감싸는게 아니라 삭제해야 한다
+ * 나중에 정말로 필요해지면 git 등 vcs 을 사용해 복원이 가능하다
+ */
 public class Reservation {
 
     private String title;
@@ -12,6 +18,7 @@ public class Reservation {
 
     private LocalDateTime alarm;
 
+    //테스트 코드에서만 사용하기 때문에 삭제 가능
     public Reservation(String title, LocalDateTime from, LocalDateTime to) {
         this.title = title;
         this.from = from;
