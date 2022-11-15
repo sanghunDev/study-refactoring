b42cf16e4d02c74ff1e989768a9262048f63cb3b
sanghunDev <bsh20057@gmail.com>
2022-11-15 12:46:14 +0900

    perf(냄새18 중재자): 서브클래스를 위임으로 바꾸기 (Replace Subclass with Delegate) - 어떤 객체의 행동이 카테고리에 따라 바뀐다면, 보통 상속을 사용해서 일반적인 로직은 슈퍼클래스에 두고 특이한 케이스에 해당하는 로직을 서브 클래스를 사용해 표현한다 - 하지만 대부분의 프로그래밍 언어에서 상속은 오직 한번만 사용할 수 있다 ( - 만약에 어떤 객체를 두가지 이상의 카테고리로 구분해야 한다면? - 위임을 사용하면 얼마든지 여러가지 이유로 여러 다른 객체로 위임을 할 수 있다 ) - 슈퍼클래스가 바뀌면 모든 서브클래스에 영향을 줄 수 있다. 따라서 슈퍼클래스를 변경할 때 서브클래스까지 신경써야 한다 ( - 만약에 서브클래스가 전혀 다른 모듈에 있다면? - 위임을 사용한다면 중간에 인터페이스를 만들어 의존성을 줄일 수 있다 ) - "상속 대신 위임을 선호하라"는 말은 결코 "상속은 나쁘다"라는 말이 아니다 (처음엔 상속을 적용하고 언제든지 이런 리팩토링을 사용해 위임으로 전환할 수 있다)

--- src/main/java/me/whiteship/refactoring/_18_middle_man/_40_replace_subclass_with_delegate/Booking.java
+++ src/main/java/me/whiteship/refactoring/_18_middle_man/_40_replace_subclass_with_delegate/Booking.java
@@ -9,13 +9,28 @@ public class Booking {
 
     protected LocalDateTime time;
 
+    protected PremiumDelegate premiumDelegate;
+    
     public Booking(Show show, LocalDateTime time) {
         this.show = show;
         this.time = time;
     }
 
+    //정적 팩토리 메서드로 객체 생성의 유연성 확보(하위 클래스 반환가능, 메서드명 명시적으로 작성 가능)
+    public static Booking createBooking(Show show, LocalDateTime time) {
+        return new Booking(show, time);
+    }
+
+    //PremiumDelegate 클래스를 사용해서 상속에 있던 클래스들을 이동, 상속 관계를 끊고 위임으로 변경
+    public static Booking createPremiumBooking(Show show, LocalDateTime time, PremiumExtra extra) {
+        Booking booking = createBooking(show, time);
+        booking.premiumDelegate = new PremiumDelegate(booking, extra);
+        return booking;
+    }
+
     public boolean hasTalkback() {
-        return this.show.hasOwnProperty("talkback") && !this.isPeakDay();
+        return (this.premiumDelegate != null) ? this.premiumDelegate.hasTalkback() :
+                this.show.hasOwnProperty("talkback") && !this.isPeakDay();
     }
 
     protected boolean isPeakDay() {
@@ -26,7 +41,10 @@ public class Booking {
     public double basePrice() {
         double result = this.show.getPrice();
         if (this.isPeakDay()) result += Math.round(result * 0.15);
-        return result;
+        return (this.premiumDelegate != null) ? this.premiumDelegate.extendBasePrice(result) : result;
     }
 
+    public boolean hasDinner() {
+        return this.premiumDelegate != null && this.premiumDelegate.hasDinner();
+    }
 }
